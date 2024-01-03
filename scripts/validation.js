d7b50ec7a37967c52f607469f3b5611e1af907e1
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-03 21:14:27 +1100

    moved translation-related functions from script.js

---
+++ scripts/validation.js
@@ -0,0 +1,15 @@
+import { translateToMorse, translateToEnglish } from "./translation.js"; 
+
+export const validateInput = (event) => {
+  const input = event.target.parentElement.previousElementSibling.value;
+
+  if (typeof input !== 'string') {
+    console.warn("Input is not of string type.");
+    return false;
+  }
+
+  let toTranslateLanguage = document.querySelector(".translate-order__to-translate-language").innerText;
+  toTranslateLanguage === "English" ? translateToMorse(input) : translateToEnglish(input);
+
+  return true;
+}
