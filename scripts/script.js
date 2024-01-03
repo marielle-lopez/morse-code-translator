f300739aa09d93f3bb579c36e42dd5e2d99218e8
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-03 22:07:14 +1100

    added copy to clipboard feature

--- scripts/script.js
+++ scripts/script.js
@@ -16,5 +16,17 @@ const swapToTranslateLanguage = () => {
   swapDisplayedToTranslateLanguage(toTranslateLanguage, otherLanguage);
 };
 
+const copyToClipboard = () => {
+  const translation = document.querySelector(".translated__textbox").value;
+  navigator.clipboard.writeText(translation);
+
+  document.querySelector(".translated__interaction-area__message").innerText = "Copied!";
+
+  setTimeout(() => {
+    document.querySelector(".translated__interaction-area__message").innerText = "";
+  }, 4000)
+}
+
 document.querySelector(".to-translate__interaction-area__translate-button").addEventListener("click", validateInput);
 document.querySelector(".translate-order__swap-button").addEventListener("click", swapToTranslateLanguage);
+document.querySelector(".translated__interaction-area__copy-button").addEventListener("click", copyToClipboard);
