ead6bfba913a76992613574dd7d300a028dbef92
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-03 19:51:33 +1100

    added basic morse to english translation functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+let toTranslateLanguage = "English";
+
 const morseCode = {
   " ": "/",
   "A": ".-",
@@ -58,18 +60,26 @@ const morseCode = {
   "¡": "--...-"
 }
 
+getKeyByValue = (object, value) => {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
 validateInput = (event) => {
-  let userInput = event.target.previousElementSibling.value;
+  let userInput = event.target.parentElement.previousElementSibling.value;
 
   if (typeof userInput !== "string") {
-    console.log("Input isn't of string type.");
+    console.log("Input isn't of string type.", typeof userInput);
     return;
+  } 
+
+  if (toTranslateLanguage === "English") {
+    translateToMorse(userInput.toUpperCase());
   } else {
-    translate(userInput.toUpperCase());
+    translateToEnglish(userInput);
   }
 };
 
-translate = (userInput) => {
+translateToMorse = (userInput) => {
   let translation = "";
   let characters = userInput.split("");
 
@@ -84,10 +94,44 @@ translate = (userInput) => {
   displayTranslation(translation);
 };
 
+translateToEnglish = (userInput) => {
+  let translation = "";
+  let signals = userInput.split(" ");
+
+  for (let i = 0; i < signals.length; i++) {
+    let translatedSignal = getKeyByValue(morseCode, signals[i]);
+    translation += translatedSignal;
+  }
+
+  displayTranslation(translation);
+};
+
 displayTranslation = (translation) => {
   document.querySelector(".translated__textbox").value = translation;
 };
 
+swapToTranslateLanguage = (event) => {
+  let otherLanguage = "";
+
+  if (toTranslateLanguage === "English") {
+    toTranslateLanguage = "Morse"
+    otherLanguage = "English";
+  } else {
+    toTranslateLanguage = "English"
+    otherLanguage = "Morse";
+  }
+
+  swapDisplayedToTranslateLanguage(otherLanguage);
+};
+
+
+swapDisplayedToTranslateLanguage = (otherLanguage) => {
+  document.querySelector(".translate-order__to-translate-language").innerText = toTranslateLanguage;
+  document.querySelector(".translate-order__translated-language").innerText = otherLanguage;
+};
+
 document
-  .querySelector(".to-translate__translate-button")
+  .querySelector(".to-translate__interaction-area__translate-button")
   .addEventListener("click", validateInput);
+
+document.querySelector(".translate-order__swap-button").addEventListener("click", swapToTranslateLanguage);
