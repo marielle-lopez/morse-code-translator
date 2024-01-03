820bc763f2619720a898e2bc90ffe35769e5dc3f
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-03 21:14:04 +1100

    moved translation-related functions from script.js

---
+++ scripts/translation.js
@@ -0,0 +1,90 @@
+import { displayTranslation } from "./dom-manipulation.js";
+
+const morseCode = {
+  " ": "/",
+  "A": ".-",
+  "B": "-...",
+  "C": "-.-.",
+  "D": "-..",
+  "E": ".",
+  "F": "..-.",
+  "G": "--.",
+  "H": "....",
+  "I": "..",
+  "J": ".---",
+  "K": "-.-",
+  "L": ".-..",
+  "M": "--",
+  "N": "-.",
+  "O": "---",
+  "P": ".--.",
+  "Q": "--.-",
+  "R": ".-.",
+  "S": "...",
+  "T": "-",
+  "U": "..-",
+  "V": "...-",
+  "W": ".--",
+  "X": "-..-",
+  "Y": "-.--",
+  "Z": "--..",
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  ".": ".-.-.-",
+  ",": "--..---",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  "_": "..--.-",
+  '"': ".-..-.",
+  "$": "...-..-",
+  "@": ".--.-.",
+  "¿": "..-.-",
+  "¡": "--...-"
+}
+
+const getKeyByValue = (object, value) => {
+  return Object.keys(object).find((key) => object[key] === value);
+}
+
+export const translateToMorse = (input) => {
+  const characters = input.toUpperCase().split("");
+
+  const translation = characters.reduce((string, character) => {
+    const signal = morseCode[character];
+    return string += signal + " ";
+  }, "")
+    .trimEnd();
+
+  displayTranslation(translation);
+  return translation;
+}
+
+export const translateToEnglish = (input) => {
+  let signals = input.split(" ");
+
+  const translation = signals.reduce((string, signal) => {
+    const translatedSignal = getKeyByValue(morseCode, signal);
+    return string += translatedSignal;
+  }, "");
+
+  displayTranslation(translation);
+  return translation;
+}
