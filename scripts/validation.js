6640775b1806be27e4c77c4a03f2552b90fd00d5
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-10 21:16:29 +1100

    used querySelector to find specific DOM element

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,15 +1,19 @@
-import { translateToMorse, translateToEnglish } from "./translation.js"; 
+import { translateToMorse, translateToEnglish } from "./translation.js";
 
 export const validateInput = (event) => {
-  const input = event.target.parentElement.previousElementSibling.value;
+  const input = document.querySelector("#toTranslate").value;
 
-  if (typeof input !== 'string') {
+  if (typeof input !== "string") {
     console.warn("Input is not of string type.");
     return false;
   }
 
-  let toTranslateLanguage = document.querySelector(".translate-order__to-translate-language").innerText;
-  toTranslateLanguage === "English" ? translateToMorse(input) : translateToEnglish(input);
+  let toTranslateLanguage = document.querySelector(
+    ".translate-order__to-translate-language"
+  ).innerText;
+  toTranslateLanguage === "English"
+    ? translateToMorse(input)
+    : translateToEnglish(input);
 
   return true;
-}
+};
