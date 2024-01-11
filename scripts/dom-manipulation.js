82b80d1bbcd178dee3b13dca0522bf377428f117
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-11 13:33:29 +1100

    moved calls for translation functions from validation function

--- scripts/dom-manipulation.js
+++ scripts/dom-manipulation.js
@@ -1,16 +1,23 @@
 import { translateToMorse, translateToEnglish } from "./translation.js";
 import { validateInput } from "./validation.js";
 
-export const swapDisplayedToTranslateLanguage = (toTranslateLanguage, otherLanguage) => {
-  document.querySelector(".translate-order__to-translate-language").innerText = toTranslateLanguage;
-  document.querySelector(".translate-order__translated-language").innerText = otherLanguage;
+export const swapDisplayedToTranslateLanguage = (
+  toTranslateLanguage,
+  otherLanguage
+) => {
+  document.querySelector(".translate-order__to-translate-language").innerText =
+    toTranslateLanguage;
+  document.querySelector(".translate-order__translated-language").innerText =
+    otherLanguage;
 };
 
 export const displayTranslation = () => {
   const input = document.querySelector("#toTranslate").value;
 
   if (validateInput(input)) {
-    const toTranslateLanguage = document.querySelector(".translate-order__to-translate-language").innerText;
+    const toTranslateLanguage = document.querySelector(
+      ".translate-order__to-translate-language"
+    ).innerText;
 
     let translation;
 
@@ -22,4 +29,3 @@ export const displayTranslation = () => {
     document.querySelector(".translated__textbox").value = translation;
   }
 };
-
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,19 +1,17 @@
 import { translateToMorse, translateToEnglish } from "./translation.js";
 
-export const validateInput = (event) => {
-  const input = document.querySelector("#toTranslate").value;
-
+export const validateInput = (input) => {
   if (typeof input !== "string") {
     console.warn("Input is not of string type.");
     return false;
   }
 
-  let toTranslateLanguage = document.querySelector(
-    ".translate-order__to-translate-language"
-  ).innerText;
-  toTranslateLanguage === "English"
-    ? translateToMorse(input)
-    : translateToEnglish(input);
+  // let toTranslateLanguage = document.querySelector(
+  //   ".translate-order__to-translate-language"
+  // ).innerText;
+  // toTranslateLanguage === "English"
+  //   ? translateToMorse(input)
+  //   : translateToEnglish(input);
 
   return true;
 };
